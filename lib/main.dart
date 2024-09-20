ed7827788885850493f1cd0e7120f91e731bf1e2
David <owolagbadavid@gmail.com>
2024-09-20 15:40:09 +0100

    Logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:inotes/firebase_options.dart';
 import 'package:inotes/views/login_view.dart';
 import 'package:inotes/views/register_view.dart';
+import 'package:inotes/views/verify_email_view.dart';
+import 'dart:developer' as dev show log;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,6 +23,8 @@ void main() async {
     },
   ));
 }
+
+enum MenuAction { logout }
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -46,15 +48,13 @@ class HomePage extends StatelessWidget {
           case ConnectionState.done:
             final user = FirebaseAuth.instance.currentUser;
 
-            // if (user == null) {
-            //   return const LoginView();
-            // } else if (user.emailVerified) {
-            //   return const Text("Logged In");
-            // } else {
-            //   return const VerfyEmailView();
-            // }
-
-            return const LoginView();
+            if (user == null) {
+              return const LoginView();
+            } else if (user.emailVerified) {
+              return const NotesView();
+            } else {
+              return const VerfyEmailView();
+            }
 
           default:
             return const Center(
@@ -64,4 +64,74 @@ class HomePage extends StatelessWidget {
       },
     );
   }
+}
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Notes"), actions: <Widget>[
+        PopupMenuButton<MenuAction>(
+          tooltip: 'Menu',
+          itemBuilder: (context) => const <PopupMenuEntry<MenuAction>>[
+            PopupMenuItem(
+              value: MenuAction.logout,
+              child: Text('Logout'),
+            ),
+          ],
+          onSelected: (value) async {
+            switch (value) {
+              case MenuAction.logout:
+                final shouldLogOut = await showLogOutDialog(context);
+
+                if (shouldLogOut) {
+                  await FirebaseAuth.instance.signOut();
+
+                  if (!context.mounted) return;
+
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil('/login/', (_) => false);
+                }
+                return;
+            }
+          },
+        )
+      ]),
+      body: const Text("Hello World"),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) async {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Log Out'),
+          content: const Text('Are you sure you want to log out?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      }).then(
+    (value) => value ?? false,
+  );
 }
--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -1,2 +1,4 @@
+// ignore_for_file: unused_import
+
 import 'utils.dart';
 import 'package:flutter/material.dart';
