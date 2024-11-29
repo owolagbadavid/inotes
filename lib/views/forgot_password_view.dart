24be412edea2f3a864e09c6bd73acdb302dd5894
David <owolagbadavid@gmail.com>
2024-11-29 10:10:04 +0100

    icons

--- lib/views/forgot_password_view.dart
+++ lib/views/forgot_password_view.dart
@@ -40,52 +40,92 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
         }
       },
       child: Scaffold(
-        appBar: AppBar(title: const Text('Forgot Password')),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: _controller,
-                autocorrect: false,
-                autofocus: true,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                ),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                style: TextButton.styleFrom(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                  backgroundColor: Theme.of(context).colorScheme.primary,
+        body: LayoutBuilder(builder: (context, constraints) {
+          double maxWidth = constraints.maxWidth < 600
+              ? constraints.maxWidth * 0.9 // For small screens
+              : 400; // For larger screens,
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: SizedBox(
+                width: maxWidth,
+                child: Card(
+                  elevation: 5,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  color: Theme.of(context).cardColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          'Reset your password',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall
+                              ?.copyWith(
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                        const SizedBox(height: 16),
+                        TextField(
+                          controller: _controller,
+                          autocorrect: false,
+                          autofocus: true,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            filled: true,
+                            // fillColor: Theme.of(context).colorScheme.surface,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Theme.of(context).dividerColor,
+                              ),
+                            ),
+                            labelText: 'Email',
+                            hintText: 'Enter your email',
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 20),
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primary,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: Text(
+                            'Send Email',
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.onPrimary),
+                          ),
+                          onPressed: () {
+                            final email = _controller.text;
+                            context
+                                .read<AuthenticationBloc>()
+                                .add(AuthEventForgotPassword(email: email));
+                          },
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            BlocProvider.of<AuthenticationBloc>(context)
+                                .add(SignOut());
+                          },
+                          child: const Text('Back to login'),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                child: Text(
-                  'Send Email',
-                  style:
-                      TextStyle(color: Theme.of(context).colorScheme.onPrimary),
-                ),
-                onPressed: () {
-                  final email = _controller.text;
-                  context
-                      .read<AuthenticationBloc>()
-                      .add(AuthEventForgotPassword(email: email));
-                },
               ),
-              TextButton(
-                onPressed: () {
-                  BlocProvider.of<AuthenticationBloc>(context).add(SignOut());
-                },
-                child: const Text('Back to login'),
-              ),
-            ],
-          ),
-        ),
+            ),
+          );
+        }),
       ),
     );
   }
